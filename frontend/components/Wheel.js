1b1aaf1b466c7425fb2153aba806c465abd68310
Victor Gordian <victorgordian103@gmail.com>
2022-03-03 11:56:41 -0800

    counter is countering

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -7,12 +7,13 @@ export  function Wheel(props) {
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+      {wheel===0?<div className="cog active" style={{ "--i": 0 }}>B</div>:<div className="cog" style={{ "--i": 0}}></div>}
+       {wheel===1?<div className="cog active" style={{ "--i": 1 }}>B</div>:<div className="cog" style={{ "--i": 1}}></div>}
+       {wheel===2?<div className="cog active" style={{ "--i": 2 }}>B</div>:<div className="cog" style={{ "--i": 2}}></div>}
+       {wheel===3?<div className="cog active" style={{ "--i": 3 }}>B</div>:<div className="cog" style={{ "--i": 3}}></div>}
+       {wheel===4?<div className="cog active" style={{ "--i": 4 }}>B</div>:<div className="cog" style={{ "--i": 4}}></div>}
+       {wheel===5?<div className="cog active" style={{ "--i": 5 }}>B</div>:<div className="cog" style={{ "--i": 5}}></div>}
+       {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" onClick={() => props.moveCounterClockwise()}>Counter clockwise</button>
